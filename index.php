abac5cb230dde6d8633245e759cafe1b8763b97b
zCaioz <112457664+zCaioz@users.noreply.github.com>
2025-07-20 23:16:16 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ require_once __DIR__ . '/controllers/AuthController.php';
 
 session_start();
 
+header("Access-Control-Allow-Origin: https://bibilioteca-frontend.netlify.app");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
